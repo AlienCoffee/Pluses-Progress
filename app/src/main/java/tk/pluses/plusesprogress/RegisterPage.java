d2b03dc093234462103590429706ff1ab9a20511
notantony <not.antony@yandex.ru>
2017-07-07 22:44:03 +0300

    register page added

---
+++ app/src/main/java/tk/pluses/plusesprogress/RegisterPage.java
@@ -0,0 +1,18 @@
+package tk.pluses.plusesprogress;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page);
+    }
+
+    public static void register(View view){
+
+    }
+}
