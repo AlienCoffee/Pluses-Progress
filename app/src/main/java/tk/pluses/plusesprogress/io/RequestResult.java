69b8e9f97cb2be0008df2c202f93107bbd71d931
Shemplo <shemplo@outlook.com>
2017-07-10 18:00:34 +0300

    Commented

--- app/src/main/java/tk/pluses/plusesprogress/io/RequestResult.java
+++ app/src/main/java/tk/pluses/plusesprogress/io/RequestResult.java
@@ -7,17 +7,22 @@ import java.util.Map;
 public class RequestResult implements Serializable {
 
     /**
+     * Response code of request.
      *
+     * Read about HTTP response codes (it's similar to them)
      * */
     public final int CODE;
 
     /**
-     *
+     * String address of host where request was sent.
      * */
     public final String HOST;
 
     /**
+     * All fields that was received from server answer.
      *
+     * Guaranteed that there are fields <i>code<i/>,
+     * <i>message</i>, <i>comment</i> and <i>cause</i>
      * */
     private Map <String, String> FIELDS;
 
@@ -27,6 +32,12 @@ public class RequestResult implements Serializable {
         this.FIELDS = new HashMap <> (fields);
     }
 
+    /**
+     * Get field value by key.
+     *
+     * If key doesn't exist or null
+     * then will be returned null
+     * */
     public String getField (String key) {
         if (key == null) { return null; }
         return FIELDS.get (key);
