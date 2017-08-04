0393cdf1e8058a4ef2b702a1b80f46f5adeb531d
Shemplo <shemplo@outlook.com>
2017-08-04 18:22:43 +0300

    Loading user data done

--- app/src/main/java/tk/pluses/plusesprogress/fragments/FragmentAuth.java
+++ app/src/main/java/tk/pluses/plusesprogress/fragments/FragmentAuth.java
@@ -1,6 +1,8 @@
 package tk.pluses.plusesprogress.fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,15 +16,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Iterator;
 
 import tk.pluses.plusesprogress.R;
 import tk.pluses.plusesprogress.RegisterPage;
 import tk.pluses.plusesprogress.io.RequestForm;
 import tk.pluses.plusesprogress.io.RequestIO;
 import tk.pluses.plusesprogress.io.RequestResult;
+import tk.pluses.plusesprogress.user.UserEntity;
 
 /**
  * Created by Андрей on 13.07.2017.
@@ -42,8 +49,8 @@ public class FragmentAuth extends Fragment implements LoaderManager.LoaderCallba
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onViewCreated (View view, Bundle savedInstanceState) {
+        super.onViewCreated (view, savedInstanceState);
 
         loginButton = (Button) view.findViewById (R.id.loginButton);
         loginButton.setOnClickListener (new View.OnClickListener () {
@@ -79,9 +86,9 @@ public class FragmentAuth extends Fragment implements LoaderManager.LoaderCallba
 
     @Override
     public void onLoadFinished (Loader <RequestResult> loader, RequestResult data) {
-        Log.i ("RegPage", data.TYPE + " (code: " + data.CODE + ")");
+        Log.i ("AuthFragment", data.TYPE + " (code: " + data.CODE + ")");
         if (data.TYPE.equals ("Error")) {
-            Log.e ("RegPage", "Message: " + data.getField ("name")
+            Log.e ("AuthFragment", "Message: " + data.getField ("name")
                     + " (comment: " + data.getField ("message") + ")");
             if (data.CODE >= 3000) {
                 String message = data.getField ("message");
@@ -90,6 +97,39 @@ public class FragmentAuth extends Fragment implements LoaderManager.LoaderCallba
                                             ? ": " + message
                                             : ""));
                 answerMessage.setTextColor (Color.RED);
+            }
+        } else {
+            if (data.HOST.equals ("http://pluses.tk/api.users.authorizeUser")) {
+                try {
+                    String message = data.getField ("message");
+                    Log.i ("AuthFragment", "Message: " + message);
+                    JSONObject json = new JSONObject (message);
+                    String token = json.getString ("token");
+                    String id    = json.getString ("id");
+                    Log.i ("AuthFragment", "Token: " + token);
+                    Log.i ("AuthFragment", "ID: " + id);
+                    UserEntity.setProperty ("token", token);
+                    UserEntity.setProperty ("id", id);
+                } catch (JSONException jsone) {}
+
+                RequestForm form = new RequestForm ("http://pluses.tk/api.users.getUserData");
+                form.addParam ("token", UserEntity.getProperty ("token"));
+                form.addParam ("user_id", UserEntity.getProperty ("id"));
+
+                Bundle args = new Bundle ();
+                args.putSerializable ("form", form);
+                getLoaderManager ().restartLoader (0, args, this);
+            } else if (data.HOST.equals ("http://pluses.tk/api.users.getUserData")) {
+                try {
+                    String message = data.getField ("message");
+                    Log.i ("AuthFragment", "Message: " + message);
+                    JSONObject json = new JSONObject (message);
+                    Iterator <String> keys = json.keys ();
+                    while (keys.hasNext ()) {
+                        String key = keys.next ();
+                        UserEntity.setProperty (key, json.getString (key));
+                    }
+                } catch (JSONException jsone) {}
             }
         }
         loginButton.setEnabled (true);
@@ -103,7 +143,7 @@ public class FragmentAuth extends Fragment implements LoaderManager.LoaderCallba
                 password = passwordField.getText ().toString ();
 
         RequestForm form = new RequestForm ("http://pluses.tk/api.users.authorizeUser");
-        form.addParam ("token", "def");
+        form.addParam ("token", UserEntity.getProperty ("token"));
         Log.i ("RegPage", phone);
         form.addParam ("phone", phone);
         try {
@@ -123,6 +163,12 @@ public class FragmentAuth extends Fragment implements LoaderManager.LoaderCallba
         }
         Log.i ("RegPage", password);
         form.addParam ("password", password);
+
+        if (UserEntity.getPreferences ().contains ("device.code")) {
+            String code = UserEntity.getPreferences ().getString ("device.code", "def");
+            form.addParam ("device_code", code);
+            Log.i ("RegPage", code);
+        }
 
         Bundle args = new Bundle ();
         args.putSerializable ("form", form);
