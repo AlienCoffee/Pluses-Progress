0227709e696b063b812608a598aa5b9750205d0b
notantony <not.antony@yandex.ru>
2017-08-04 11:24:22 +0300

    save progress

---
+++ app/src/main/java/tk/pluses/plusesprogress/fragments/ListFragmentTest.java
@@ -0,0 +1,17 @@
+package tk.pluses.plusesprogress.fragments;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class ListFragmentTest extends ListFragment{
+    String data[] = new String[] { "qwe", "qqq", "sss", "aaa" };
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, data);
+        setListAdapter(adapter);
+    }
+}
