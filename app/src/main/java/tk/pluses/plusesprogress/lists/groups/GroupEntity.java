a55dca4396b63b7585f796e9ab04144b0c398e43
Shemplo <shemplo@outlook.com>
2017-08-05 07:55:42 +0300

    Drawing of groups list done

---
+++ app/src/main/java/tk/pluses/plusesprogress/lists/groups/GroupEntity.java
@@ -0,0 +1,18 @@
+package tk.pluses.plusesprogress.lists.groups;
+
+/**
+ * Created by Андрей on 05.08.2017.
+ */
+
+public class GroupEntity {
+
+    /**
+     *
+     * */
+    public final int ID;
+
+    public GroupEntity (int id) {
+        this.ID = id;
+    }
+
+}
