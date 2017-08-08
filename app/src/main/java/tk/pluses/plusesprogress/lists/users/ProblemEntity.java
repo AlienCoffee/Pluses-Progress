0f58c8aff547d8b79d33a481580fc59178d54e30
Shemplo <shemplo@outlook.com>
2017-08-08 23:17:43 +0300

    Users list continued

--- app/src/main/java/tk/pluses/plusesprogress/lists/users/ProblemEntity.java
+++ app/src/main/java/tk/pluses/plusesprogress/lists/users/ProblemEntity.java
@@ -5,4 +5,15 @@ package tk.pluses.plusesprogress.lists.users;
  */
 
 public class ProblemEntity {
+
+    public final int INDEX;
+    public final String NAME;
+    public final boolean RATING;
+
+    public ProblemEntity (int index, String name, int rating) {
+        this.RATING = rating == 1 ? true : false;
+        this.INDEX = index;
+        this.NAME = name;
+    }
+
 }
